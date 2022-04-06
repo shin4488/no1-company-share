97891902b26d71a1eda03327d97353975f230ee8
shin4488 <efshinya48@gmail.com>
2022-04-06 22:59:56 +0900

    add custom linting & formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,16 @@ module.exports = {
   ],
   plugins: [],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'no-console': 'warn',
+    'no-extra-semi': 'warn',
+    'dot-notation': 'warn',
+    'prefer-const': 'error',
+    'no-unreachable-loop': 'error',
+    'no-var': 'error',
+    // 「if () return;」のような記述は「{}」つきの記述とする
+    curly: 'error',
+    // 「(obj?.foo)();」->「(obj?.foo)?.();」
+    'no-unsafe-optional-chaining': 'error',
+  },
 }
