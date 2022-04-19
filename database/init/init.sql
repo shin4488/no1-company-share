4cdd6547c3dfa41a0a3c7b2ec01530e43661beab
shin4488 <efshinya48@gmail.com>
2022-04-19 15:56:53 +0900

    change data type from char to varchar change length of post table id column

--- database/init/init.sql
+++ database/init/init.sql
@@ -7,14 +7,14 @@ GRANT ALL PRIVILEGES ON DATABASE company_share TO company_share_admin;
 -- ユーザ：ユーザID
 CREATE TABLE public.user_master (
   -- firebaseのauthユーザid
-  id CHAR(28) PRIMARY KEY,
+  id VARCHAR(28) PRIMARY KEY,
   created_at TIMESTAMP WITH TIME ZONE,
   updated_at TIMESTAMP WITH TIME ZONE
 );
 -- 会社マスタ：法人番号、会社名、ホームページURL
 CREATE TABLE public.company_master (
   -- 予約語と被らないようにするために、companyをプレフィックスとしてつけている
-  company_number CHAR(13) PRIMARY KEY,
+  company_number VARCHAR(13) PRIMARY KEY,
   -- とりあえず100文字あれば入るだろうという想定
   company_name VARCHAR(100),
   -- Edgeが2000文字までの対応のため、最大2100文字としている
@@ -34,9 +34,9 @@ CREATE TABLE public.division_master (
 
 -- 投稿：投稿ID、法人番号、ユーザID、削除済み、報告済み、報告内容、備考
 CREATE TABLE public.post (
-  id CHAR(10) PRIMARY KEY,
-  company_number CHAR(13) REFERENCES public.company_master (company_number) ON DELETE RESTRICT,
-  user_id CHAR(28) REFERENCES public.user_master (id) ON DELETE RESTRICT,
+  id VARCHAR(58) PRIMARY KEY,
+  company_number VARCHAR(13) REFERENCES public.company_master (company_number) ON DELETE RESTRICT,
+  user_id VARCHAR(28) REFERENCES public.user_master (id) ON DELETE RESTRICT,
   is_deleted BOOLEAN,
   is_reported BOOLEAN,
   report_detail VARCHAR(300),
@@ -47,7 +47,7 @@ CREATE TABLE public.post (
 -- 投稿詳細：投稿詳細ID、投稿ID、一位内容、一位区分
 CREATE TABLE public.post_detail (
   id INTEGER,
-  post_id CHAR(10) REFERENCES public.post (id) ON DELETE RESTRICT,
+  post_id VARCHAR(48) REFERENCES public.post (id) ON DELETE RESTRICT,
   no1_content VARCHAR(100),
   no1_division INTEGER REFERENCES public.division_master (id) ON DELETE RESTRICT,
   created_at TIMESTAMP WITH TIME ZONE,
@@ -56,8 +56,8 @@ CREATE TABLE public.post_detail (
 );
 -- お気に入り：投稿ID、お気に入りユーザID
 CREATE TABLE public.bookmark (
-  post_id CHAR(10) REFERENCES public.post (id) ON DELETE RESTRICT,
-  user_id CHAR(28) REFERENCES public.user_master (id) ON DELETE RESTRICT,
+  post_id VARCHAR(10) REFERENCES public.post (id) ON DELETE RESTRICT,
+  user_id VARCHAR(28) REFERENCES public.user_master (id) ON DELETE RESTRICT,
   created_at TIMESTAMP WITH TIME ZONE,
   updated_at TIMESTAMP WITH TIME ZONE,
   PRIMARY KEY (post_id, user_id)
