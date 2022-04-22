799b018f2d2855720b3097a18a2b16ea3a2ce0ee
shin4488 <efshinya48@gmail.com>
2022-04-22 19:58:40 +0900

    #6 add definition file of default component

---
+++ application/front/layouts/def/default/defaultData.ts
@@ -0,0 +1,18 @@
+/**
+ * サイドバーの要素
+ */
+export interface SidebarItem {
+  icon: string;
+  title: string;
+  to: string;
+}
+
+/**
+ * DefaultページのVueデータ
+ */
+export interface DefaultData {
+  isDrawerOpened: boolean;
+  isDrawerMini: boolean;
+  title: string;
+  sideBarItems: SidebarItem[];
+}
