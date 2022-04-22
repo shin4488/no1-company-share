174c43332167e1a6b9d9fa63288480a74e0a2c2e
shin4488 <efshinya48@gmail.com>
2022-04-22 23:10:44 +0900

    #6 change plugins dir structure by features

--- application/nuxt.config.js
+++ application/nuxt.config.js
@@ -34,7 +34,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['~/plugins/axios.ts'],
+  plugins: ['~/plugins/axios/axios.ts'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
