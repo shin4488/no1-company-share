eaf5daf931bcb96a5d91f55679edd572a2add643
shin4488 <efshinya48@gmail.com>
2022-04-28 19:02:19 +0900

    #24 move common process to cardlist component add lodash to package json

--- application/front/components/SharedPostCardList.vue
+++ application/front/components/SharedPostCardList.vue
@@ -31,6 +31,9 @@
         />
       </v-col>
     </v-row>
+
+    <SharedPostDialog ref="sharedPostDialog" :no1-divisions="no1Divisions" />
+    <AddIconFixedButton @click="onClickedAddPostButton" />
   </div>
 </template>
 
@@ -38,6 +41,11 @@
 import Vue, { PropType } from 'vue';
 import { SelectItem } from '@f/definition/common/selectItem';
 import { SharedPost } from '@f/definition/common/sharedPost';
+import SharedPostDialog from '@f/components/SharedPostDialog.vue';
+import { SharedPostDialogParameter } from '@f/definition/components/sharedPostDialog/parameter';
+import { SharedPostDialogResult } from '@f/definition/components/sharedPostDialog/result';
+import { StringUtil } from '@c/util/stringUtil';
+import { ArrayUtil } from '@c/util/arrayUtil';
 
 export default Vue.extend({
   name: 'SharedPostCardList',
@@ -58,20 +66,189 @@ export default Vue.extend({
     },
   },
   methods: {
-    onAddedBookmark(context: { postId: string }): void {
-      this.$emit('add-bookmark', context);
+    /**
+     * お気に入り追加処理
+     */
+    onAddedBookmark({ postId }: { postId: string }): void {
+      const bookmarkedPostIndex = this.getTargetPostIndex(postId);
+      if (bookmarkedPostIndex === -1) {
+        return;
+      }
+
+      // TODO:お気に入り追加処理呼び出し
+      const clonedPosts = this.$cloner.deepClone(this.value);
+      clonedPosts[bookmarkedPostIndex].numberOfBookmarks++;
+      clonedPosts[bookmarkedPostIndex].isBookmarkedByLoginUser = true;
+      this.$emit('input', clonedPosts);
     },
-    onRemovedBookmark(context: { postId: string }): void {
-      this.$emit('remove-bookmark', context);
+    /**
+     * お気に入り削除処理
+     */
+    onRemovedBookmark({ postId }: { postId: string }): void {
+      const bookmarkedPostIndex = this.getTargetPostIndex(postId);
+      if (bookmarkedPostIndex === -1) {
+        return;
+      }
+
+      // TODO:お気に入り削除処理呼び出し
+      const clonedPosts = this.$cloner.deepClone(this.value);
+      clonedPosts[bookmarkedPostIndex].numberOfBookmarks--;
+      clonedPosts[bookmarkedPostIndex].isBookmarkedByLoginUser = false;
+      this.$emit('input', clonedPosts);
     },
+    /**
+     * 通報確定処理
+     */
     onConfirmedReport(context: { postId: string }): void {
       this.$emit('confirm-report', context);
     },
-    onEdited(context: { postId: string }): void {
-      this.$emit('click-edit', context);
+    /**
+     * 投稿削除処理
+     */
+    onDeleted({ postId }: { postId: string }): void {
+      const deletedPostIndex = this.getTargetPostIndex(postId);
+      if (deletedPostIndex === -1) {
+        return;
+      }
+
+      const clonedPosts = this.$cloner.deepClone(this.value);
+      // TODO:投稿削除処理呼び出し
+      clonedPosts.splice(deletedPostIndex, 1);
+      this.$emit('input', clonedPosts);
     },
-    onDeleted(context: { postId: string }): void {
-      this.$emit('confirm-delete', context);
+    /**
+     * 投稿編集時処理
+     */
+    async onEdited({ postId }: { postId: string }): Promise<void> {
+      const editedPostIndex = this.getTargetPostIndex(postId);
+      if (editedPostIndex === -1) {
+        return;
+      }
+
+      const clonedPosts = this.$cloner.deepClone(this.value);
+      const clonedEditedPost = clonedPosts[editedPostIndex];
+      const parameter: SharedPostDialogParameter = {
+        postId: clonedEditedPost.postId,
+        companyNumber: clonedEditedPost.companyNumber,
+        companyName: clonedEditedPost.companyName,
+        companyHomepageUrl: clonedEditedPost.companyHomepageUrl,
+        companyImageUrl: clonedEditedPost.companyImageUrl,
+        remarks: clonedEditedPost.remarks,
+        updatedAt: clonedEditedPost.updatedAt,
+        postDetails: clonedEditedPost.postDetails.map((x) => ({
+          postDetailId: x.postDetailId,
+          no1Content: x.no1Content,
+          no1Division: x.no1Division,
+        })),
+      };
+      const result = await this.openSharedPostDialog(parameter);
+      if (result === undefined) {
+        return;
+      }
+
+      clonedEditedPost.postId = result.postId;
+      clonedEditedPost.companyNumber = result.companyNumber;
+      clonedEditedPost.companyName = result.companyName;
+      clonedEditedPost.companyHomepageUrl = result.companyHomepageUrl;
+      clonedEditedPost.companyImageUrl = result.companyImageUrl;
+      clonedEditedPost.remarks = result.remarks;
+      clonedEditedPost.updatedAt = result.updatedAt;
+      clonedEditedPost.postDetails = result.postDetails.map((x) => ({
+        postDetailId: x.postDetailId,
+        no1Content: x.no1Content,
+        no1Division: x.no1Division,
+      }));
+      clonedPosts[editedPostIndex] = clonedEditedPost;
+      this.$emit('input', clonedPosts);
+    },
+    /**
+     * 新規投稿追加ボタン押下処理
+     */
+    async onClickedAddPostButton(): Promise<void> {
+      const firebaseLoginUserId =
+        this.$accessor.firebaseAuthorization.userIdComputed;
+      if (StringUtil.isEmpty(firebaseLoginUserId)) {
+        this.$accessor.snackBarError.open('投稿するにはログインしてください。');
+        return;
+      }
+
+      // №1区分は選択肢の一番上を初期値とする
+      let firstNo1Division = '';
+      if (ArrayUtil.isNotEmpty(this.no1Divisions)) {
+        firstNo1Division = this.no1Divisions[0].value;
+      }
+
+      const parameter: SharedPostDialogParameter = {
+        postId: '',
+        companyNumber: '',
+        companyName: '',
+        companyHomepageUrl: '',
+        companyImageUrl: '',
+        remarks: '',
+        updatedAt: '',
+        postDetails: [
+          {
+            postDetailId: null,
+            no1Content: '',
+            no1Division: firstNo1Division,
+          },
+        ],
+      };
+      const result = await this.openSharedPostDialog(parameter);
+      if (result === undefined) {
+        return;
+      }
+
+      const newPost: SharedPost = {
+        postId: result.postId,
+        companyNumber: result.companyNumber,
+        companyName: result.companyName,
+        companyHomepageUrl: result.companyHomepageUrl,
+        companyImageUrl: result.companyImageUrl,
+        postingUserId: StringUtil.ifEmpty(
+          this.$accessor.firebaseAuthorization.userIdComputed,
+        ),
+        postingUserName: StringUtil.ifEmpty(
+          this.$fireModule.auth().currentUser?.displayName,
+        ),
+        postingUserIcomImageUrl: StringUtil.ifEmpty(
+          this.$fireModule.auth().currentUser?.photoURL,
+        ),
+        isBookmarkedByLoginUser: false,
+        numberOfBookmarks: 0,
+        remarks: result.remarks,
+        updatedAt: result.updatedAt,
+        postDetails: result.postDetails.map((x) => ({
+          postDetailId: x.postDetailId,
+          no1Content: x.no1Content,
+          no1Division: x.no1Division,
+        })),
+      };
+
+      // 新規作成したデータは最新投稿であるため、最上位に表示
+      const clonedPosts = this.$cloner.deepClone(this.value);
+      clonedPosts.unshift(newPost);
+      this.$emit('input', clonedPosts);
+    },
+
+    /**
+     * 投稿編集ダイアログ起動
+     */
+    async openSharedPostDialog(
+      parameter: SharedPostDialogParameter,
+    ): Promise<SharedPostDialogResult | void> {
+      const sharedPostDialog = this.$refs.sharedPostDialog as InstanceType<
+        typeof SharedPostDialog
+      >;
+
+      const result = await sharedPostDialog.open(parameter);
+      return result;
+    },
+    /**
+     * 投稿IDから投稿要素番号を取得
+     */
+    getTargetPostIndex(keyPostId: string): number {
+      return this.value.findIndex((x) => x.postId === keyPostId);
     },
   },
 });
--- application/front/pages/home/index.vue
+++ application/front/pages/home/index.vue
@@ -3,20 +3,13 @@
     <SharedPostCardList
       v-model="sharedCompanyPosts"
       :no1-divisions="no1Divisions"
-      @add-bookmark="onAddedBookmark"
-      @remove-bookmark="onRemovedBookmark"
       @confirm-report="onConfirmedReport"
-      @click-edit="onEdited"
-      @confirm-delete="onDeleted"
     />
-    <SharedPostDialog ref="sharedPostDialog" :no1-divisions="no1Divisions" />
-
     <LoadMoreButton
       v-if="isLoadMoreButtonShown"
       text="さらに表示"
       @click="onClickedLoadMoreButton"
     />
-    <AddIconFixedButton @click="onClickedAddPostButton" />
   </div>
 </template>
 
@@ -32,10 +25,6 @@ import { ArrayUtil } from '@c/util/arrayUtil';
 import { postFetchedLimit } from '@f/common/constant/sharedPost';
 import { SharedPost } from '@f/definition/common/sharedPost';
 import { SelectItem } from '@f/definition/common/selectItem';
-import SharedPostDialog from '@f/components/SharedPostDialog.vue';
-import { SharedPostDialogParameter } from '@f/definition/components/sharedPostDialog/parameter';
-import { SharedPostDialogResult } from '@f/definition/components/sharedPostDialog/result';
-import { StringUtil } from '@c/util/stringUtil';
 
 export default Vue.extend({
   name: 'HomePage',
@@ -80,161 +69,10 @@ export default Vue.extend({
   },
   methods: {
     /**
-     * お気に入り追加時処理
-     */
-    onAddedBookmark({ postId }: { postId: string }): void {
-      console.log(postId);
-      const bookmarkedPostIndex = this.sharedCompanyPosts.findIndex(
-        (x) => x.postId === postId,
-      );
-      if (bookmarkedPostIndex === -1) {
-        return;
-      }
-
-      // TODO:お気に入り登録処理
-      this.sharedCompanyPosts[bookmarkedPostIndex].numberOfBookmarks++;
-      this.sharedCompanyPosts[bookmarkedPostIndex].isBookmarkedByLoginUser =
-        true;
-    },
-    /**
-     * お気に入り解除時処理
-     */
-    onRemovedBookmark({ postId }: { postId: string }): void {
-      console.log(postId);
-      const bookmarkedPostIndex = this.sharedCompanyPosts.findIndex(
-        (x) => x.postId === postId,
-      );
-      if (bookmarkedPostIndex === -1) {
-        return;
-      }
-
-      // TODO:お気に入り登録処理
-      this.sharedCompanyPosts[bookmarkedPostIndex].numberOfBookmarks--;
-      this.sharedCompanyPosts[bookmarkedPostIndex].isBookmarkedByLoginUser =
-        false;
-    },
-    /**
      * 投稿通報時処理
      */
     onConfirmedReport({ postId }: { postId: string }): void {
       console.log(postId);
-    },
-    /**
-     * 投稿編集時処理
-     */
-    async onEdited({ postId }: { postId: string }): Promise<void> {
-      const editedPostIndex = this.sharedCompanyPosts.findIndex(
-        (x) => x.postId === postId,
-      );
-      if (editedPostIndex === -1) {
-        return;
-      }
-
-      const editedPost = this.sharedCompanyPosts[editedPostIndex];
-      const parameter: SharedPostDialogParameter = {
-        postId: editedPost.postId,
-        companyNumber: editedPost.companyNumber,
-        companyName: editedPost.companyName,
-        companyHomepageUrl: editedPost.companyHomepageUrl,
-        companyImageUrl: editedPost.companyImageUrl,
-        remarks: editedPost.remarks,
-        updatedAt: editedPost.updatedAt,
-        postDetails: editedPost.postDetails.map((x) => ({
-          postDetailId: x.postDetailId,
-          no1Content: x.no1Content,
-          no1Division: x.no1Division,
-        })),
-      };
-      const result = await this.openSharedPostDialog(parameter);
-      if (result === undefined) {
-        return;
-      }
-
-      console.log(result);
-      editedPost.postId = result.postId;
-      editedPost.companyNumber = result.companyNumber;
-      editedPost.companyName = result.companyName;
-      editedPost.companyHomepageUrl = result.companyHomepageUrl;
-      editedPost.companyImageUrl = result.companyImageUrl;
-      editedPost.remarks = result.remarks;
-      editedPost.updatedAt = result.updatedAt;
-      editedPost.postDetails = result.postDetails.map((x) => ({
-        postDetailId: x.postDetailId,
-        no1Content: x.no1Content,
-        no1Division: x.no1Division,
-      }));
-      this.sharedCompanyPosts[editedPostIndex] = editedPost;
-    },
-    /**
-     * 投稿削除時処理
-     */
-    onDeleted({ postId }: { postId: string }): void {
-      console.log(postId);
-    },
-    /**
-     * 新規投稿追加ボタン押下処理
-     */
-    async onClickedAddPostButton(): Promise<void> {
-      const firebaseLoginUserId =
-        this.$accessor.firebaseAuthorization.userIdComputed;
-      if (StringUtil.isEmpty(firebaseLoginUserId)) {
-        this.$accessor.snackBarError.open('投稿するにはログインしてください。');
-        return;
-      }
-
-      let firstNo1Division = '';
-      if (ArrayUtil.isNotEmpty(this.no1Divisions)) {
-        firstNo1Division = this.no1Divisions[0].value;
-      }
-
-      const parameter: SharedPostDialogParameter = {
-        postId: '',
-        companyNumber: '',
-        companyName: '',
-        companyHomepageUrl: '',
-        companyImageUrl: '',
-        remarks: '',
-        updatedAt: '',
-        postDetails: [
-          {
-            postDetailId: null,
-            no1Content: '',
-            no1Division: firstNo1Division,
-          },
-        ],
-      };
-      const result = await this.openSharedPostDialog(parameter);
-      if (result === undefined) {
-        return;
-      }
-
-      const newPost: SharedPost = {
-        postId: result.postId,
-        companyNumber: result.companyNumber,
-        companyName: result.companyName,
-        companyHomepageUrl: result.companyHomepageUrl,
-        companyImageUrl: result.companyImageUrl,
-        postingUserId: StringUtil.ifEmpty(
-          this.$accessor.firebaseAuthorization.userIdComputed,
-        ),
-        postingUserName: StringUtil.ifEmpty(
-          this.$fireModule.auth().currentUser?.displayName,
-        ),
-        postingUserIcomImageUrl: StringUtil.ifEmpty(
-          this.$fireModule.auth().currentUser?.photoURL,
-        ),
-        isBookmarkedByLoginUser: false,
-        numberOfBookmarks: 0,
-        remarks: result.remarks,
-        updatedAt: result.updatedAt,
-        postDetails: result.postDetails.map((x) => ({
-          postDetailId: x.postDetailId,
-          no1Content: x.no1Content,
-          no1Division: x.no1Division,
-        })),
-      };
-      // 新規作成したデータは最新投稿であるため、最上位に表示
-      this.sharedCompanyPosts.unshift(newPost);
     },
     /**
      * さらに表示処理ボタン押下処理
@@ -265,16 +103,6 @@ export default Vue.extend({
         this.isLoadMoreButtonShown =
           sharedCompanyPosts.length === postFetchedLimit;
       });
-    },
-    async openSharedPostDialog(
-      parameter: SharedPostDialogParameter,
-    ): Promise<SharedPostDialogResult | void> {
-      const sharedPostDialog = this.$refs.sharedPostDialog as InstanceType<
-        typeof SharedPostDialog
-      >;
-
-      const result = await sharedPostDialog.open(parameter);
-      return result;
     },
   },
 });
