c2f90de7f0aede8edd01243696fc4b68f7cbc458
shin4488 <efshinya48@gmail.com>
2022-05-05 16:40:14 +0900

    #29 change table name from post to shared_post remove unsed data insertion

--- database/init/init.sql
+++ database/init/init.sql
@@ -34,7 +34,7 @@ CREATE TABLE public.division_master (
 );
 
 -- 投稿：投稿ID、法人番号、ユーザID、削除済み、報告済み、報告内容、備考
-CREATE TABLE public.post (
+CREATE TABLE public.shared_post (
   id VARCHAR(58) PRIMARY KEY,
   company_number VARCHAR(13) REFERENCES public.company_master (company_number) ON DELETE RESTRICT,
   user_id VARCHAR(28) REFERENCES public.user_master (id) ON DELETE RESTRICT,
@@ -46,22 +46,22 @@ CREATE TABLE public.post (
   updated_at TIMESTAMP WITH TIME ZONE
 );
 -- 投稿詳細：投稿詳細ID、投稿ID、一位内容、一位区分
-CREATE TABLE public.post_detail (
+CREATE TABLE public.shared_post_detail (
   id INTEGER,
-  post_id VARCHAR(48) REFERENCES public.post (id) ON DELETE RESTRICT,
+  shared_post_id VARCHAR(48) REFERENCES public.shared_post (id) ON DELETE RESTRICT,
   no1_content VARCHAR(100),
   no1_division INTEGER REFERENCES public.division_master (id) ON DELETE RESTRICT,
   created_at TIMESTAMP WITH TIME ZONE,
   updated_at TIMESTAMP WITH TIME ZONE,
-  PRIMARY KEY (id, post_id)
+  PRIMARY KEY (id, shared_post_id)
 );
 -- お気に入り：投稿ID、お気に入りユーザID
 CREATE TABLE public.bookmark (
-  post_id VARCHAR(10) REFERENCES public.post (id) ON DELETE RESTRICT,
+  shared_post_id VARCHAR(10) REFERENCES public.shared_post (id) ON DELETE RESTRICT,
   user_id VARCHAR(28) REFERENCES public.user_master (id) ON DELETE RESTRICT,
   created_at TIMESTAMP WITH TIME ZONE,
   updated_at TIMESTAMP WITH TIME ZONE,
-  PRIMARY KEY (post_id, user_id)
+  PRIMARY KEY (shared_post_id, user_id)
 );
 
 -- 本番時の事前データ
@@ -77,30 +77,4 @@ INSERT INTO public.division_master (
 ), (
   'NO1_DIVISION',
   '福井一'
-);
-
--- 開発時の事前データ
-INSERT INTO public.user_master (id) VALUES (
-  'cDYrCmleUFhpDok0klZxvCpZVJt-'
-);
-INSERT INTO public.company_master (
-  company_number,
-  company_name,
-  homepage_url
-) VALUES (
-  '1210001013425',
-  '酒井化学株式会社',
-  'http://example.com'
-), (
-  '1210002013036',
-  '有限会社酒井制御',
-  'http://example.com'
-), (
-  '3210001001361',
-  '株式会社酒井建設',
-  'http://www.sakai-kensetsu1925.co.jp/'
-), (
-  '4210001017778',
-  '酒井石材工業株式会社',
-  'http://sakai.tokyosekizai.jp/'
 );
