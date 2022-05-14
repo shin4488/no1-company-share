bfa3428bc348cd7e81f0dbbe42c0c81f7db1144e
shin4488 <efshinya48@gmail.com>
2022-05-14 13:06:47 +0900

    #32 add my post page

--- application/front/pages/my-post/index.vue
+++ application/front/pages/my-post/index.vue
@@ -1,29 +1,109 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        this is my post component.
-        <input v-model="message" type="text" />
-      </v-card>
-    </v-col>
-  </v-row>
+  <div>
+    <SharedPostCardList
+      v-model="sharedPosts"
+      message-if-no-data="まだ自分の投稿がありません。最初の投稿をしてみよう！"
+      :no1-divisions="no1Divisions"
+    />
+    <LoadMoreButton
+      v-if="isLoadMoreButtonShown"
+      text="さらに表示"
+      @click="onClickedLoadMoreButton"
+    />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import { Context } from '@nuxt/types';
+import { SelectItem } from '@f/definition/common/selectItem';
+import { AjaxHelper } from '@f/common/ajax/ajaxHelper';
+import { SharedPostHandler } from '@f/common/ajax/sharedPostHandler';
+import { SharedPost } from '@f/definition/common/sharedPost';
+import { MyPostPageData } from '@f/definition/pages/myPost/data';
+import { SharedPostGetReponse } from '@f/definition/pages/common/apiSpec/sharedPostGetResponse';
+import { postFetchedLimit } from '@f/common/constant/sharedPost';
+import { No1DivisionSelectItemGetResponse } from '@s/feature/division/definition/no1DivisionSelectItemGetResponse';
 import { PageHeadData } from '@f/definition/pages/common/headData';
 
 export default Vue.extend({
   name: 'MyPostPage',
-  data: () => {
+  async asyncData({ $axios, $accessor }: Context) {
+    let sharedPosts: SharedPost[] = [];
+    let no1Divisions: SelectItem[] = [];
+
+    // コンポーネントマウント前はストアアクセス不可のためスピナーは表示されない
+    await $accessor.spinnerOverlay.open(async () => {
+      const no1DivisionsReponse =
+        await AjaxHelper.get<No1DivisionSelectItemGetResponse>(
+          $axios,
+          '/divisions/no1/',
+        );
+      no1Divisions = SharedPostHandler.handleDivisionResponse(
+        no1DivisionsReponse?.no1DivisionSelectItems,
+      );
+
+      const response = await AjaxHelper.get<SharedPostGetReponse>(
+        $axios,
+        '/localhost/shared-posts/',
+        {
+          limit: postFetchedLimit,
+          // 初回読み込み時は1ページ目であるため、取得基準時刻は無し
+          baseDateTime: null,
+          isMyPostOnly: true,
+        },
+      );
+      const results = SharedPostHandler.handleResponse(response);
+      sharedPosts = results.sharedPosts;
+    });
+
+    // 1度に全データ取得しきれない場合は、さらに表示ボタンを表示
+    const isLoadMoreButtonShown = sharedPosts.length === postFetchedLimit;
+    const oldBaseDateTime = SharedPostHandler.getOldBaseDateTime(sharedPosts);
+
     return {
-      message: 'aaaaaa',
+      sharedPosts,
+      no1Divisions,
+      isLoadMoreButtonShown,
+      oldBaseDateTime,
+    };
+  },
+  data(): MyPostPageData {
+    return {
+      sharedPosts: [],
+      no1Divisions: [],
+      isLoadMoreButtonShown: false,
+      oldBaseDateTime: '',
     };
   },
   head(): PageHeadData {
     return {
       title: 'マイ投稿',
     };
+  },
+  methods: {
+    /**
+     * さらに表示処理ボタン押下処理
+     */
+    async onClickedLoadMoreButton(): Promise<void> {
+      await this.$accessor.spinnerOverlay.open(async () => {
+        const response = await AjaxHelper.get<SharedPostGetReponse>(
+          this.$axios,
+          '/localhost/shared-posts/',
+          {
+            limit: postFetchedLimit,
+            baseDateTime: this.oldBaseDateTime,
+            isMyPostOnly: true,
+          },
+        );
+
+        const { sharedPosts } = SharedPostHandler.handleResponse(response);
+        this.sharedPosts.push(...sharedPosts);
+        this.isLoadMoreButtonShown = sharedPosts.length === postFetchedLimit;
+        this.oldBaseDateTime =
+          SharedPostHandler.getOldBaseDateTime(sharedPosts);
+      });
+    },
   },
 });
 </script>
