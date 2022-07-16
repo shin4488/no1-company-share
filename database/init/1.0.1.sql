007e2ce429adf04bfc129f4ba1f1bcee2f4e1765
shin4488 <efshinya48@gmail.com>
2022-07-16 18:56:16 +0900

    add: add sql file for company master image url this file will be used to change table definition in the production server

--- database/init/1.0.1.sql
+++ database/init/1.0.1.sql
@@ -2,6 +2,4 @@
 
 ALTER TABLE public.company_master
 -- https://kyogom.com/tech/design/maxlength/
-ADD COLUMN address VARCHAR(150),
-ADD COLUMN latitude numeric(9, 7),
-ADD COLUMN longitude numeric(10, 7);
+ADD COLUMN image_url VARCHAR(2100);
